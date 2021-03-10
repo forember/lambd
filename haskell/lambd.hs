0abc6022112a65172f5b391dde6ef512265b71a8
Embers McKinney <em@embermckinney.com>
2021-03-10 11:24:40 -0500

    Finished haskell syntax code

--- haskell/lambd.hs
+++ haskell/lambd.hs
@@ -4,6 +4,8 @@ import qualified Data.Set as Set
 import Text.Regex.TDFA
 import Text.Show
 
+-- Syntax ---------------------------------------------------------------------
+
 tokenize :: String -> [String]
 tokenize s = getAllTextMatches (s =~ "[@.()]|[^[:space:]@.()]+")
 
@@ -11,9 +13,10 @@ data ParExpression = Tok String
                    | Par [ParExpression]
   deriving (Eq, Show)
 
-prettyShow :: ParExpression -> String
-prettyShow (Tok tok) = tok
-prettyShow (Par exprs) = "(" ++ (intercalate " " (map prettyShow exprs)) ++ ")"
+prettyParExpr :: ParExpression -> String
+prettyParExpr (Tok tok) = tok
+prettyParExpr (Par exprs) =
+  "(" ++ (intercalate " " (map prettyParExpr exprs)) ++ ")"
 
 isLess :: Maybe Int -> Maybe Int -> Bool
 isLess Nothing Nothing = False
@@ -36,6 +39,7 @@ parseParTree toks
 data Expression = Var String
                 | Appl Expression Expression
                 | Abst String Expression
+  deriving (Eq, Show)
 
 isVar :: String -> Bool
 isVar tok = (tok =~ "^[^[:space:]@.()]+$")
@@ -62,16 +66,16 @@ churchNum :: Int -> Expression
 churchNum num = Abst "f" (Abst "x" (churchNumBody num))
 
 applyChurchNums :: [String] -> Expression -> Expression
-applyChurchNums vars expr
-  | null vars = expr
+applyChurchNums vars body
+  | null vars = body
   | otherwise =
     let var = head vars
     in  applyChurchNums (tail vars)
           (if var == "1"
-           then Appl (Abst var expr) (Abst "f" (Var "f"))
+           then Appl (Abst var body) (Abst "f" (Var "f"))
            else if isDigit var
-           then churchNum (read var)
-           else expr)
+           then Appl (Abst var body) (churchNum (read var))
+           else body)
 
 genListBody :: String -> [ParExpression] -> Expression
 genListBody endName parExprs
@@ -82,12 +86,25 @@ genListBody endName parExprs
 genList :: String -> [ParExpression] -> Expression
 genList endName parExprs = Abst endName (genListBody endName parExprs)
 
+genAbstraction :: [ParExpression] -> Expression -> Expression
+genAbstraction vars body
+  | null vars = body
+  | otherwise =
+    let Tok name = head vars
+    in  Abst name (genAbstraction (tail vars) body)
+
+genApplication :: [Expression] -> Expression
+genApplication [expr] = expr
+genApplication [exprA, exprB] = Appl exprA exprB
+genApplication exprs = Appl (genApplication (init exprs)) (last exprs)
+
 parseParExpr :: [ParExpression] -> Expression
+parseParExpr [Par tree] = parseParExpr tree
 -- Variable
 parseParExpr [Tok tok] =
   if isVar tok
     then Var tok
-    else parseParExpr([Tok tok])
+    else parseParExpr [Tok tok]
 -- Extensions
 parseParExpr ((Tok "@"):(Tok "@"):(Tok "b"):tree) =
   let (extra, body) = splitTree "." tree
@@ -119,4 +136,51 @@ parseParExpr ((Tok "@"):(Tok "@"):(Tok "n"):tree) =
 -- Abstraction
 parseParExpr ((Tok "@"):tree) =
   let (extra, body) = splitTree "." tree
-  in  parseParExpr body -- TODO
+  in  genAbstraction extra (parseParExpr body)
+-- Application
+parseParExpr tree =
+  if elem (Tok "@") tree
+  then let (beforeLambda, afterLambda) = splitTree "@" tree
+           (extra, body) = splitTree "." afterLambda
+       in  genApplication ((map (\p -> parseParExpr [p]) beforeLambda
+                        ++ [genAbstraction extra (parseParExpr body)]))
+  else genApplication (map (\p -> parseParExpr [p]) tree)
+
+unparse :: Expression -> String
+unparse (Var name) = name
+unparse (Abst name expr) = "@" ++ name ++ "." ++ (unparse expr)
+unparse (Appl (Abst nameA exprA) (Var nameB)) =
+  "(" ++ (unparse (Abst nameA exprA)) ++ ")" ++ nameB
+unparse (Appl (Abst nameA exprA) exprB) =
+  "(" ++ (unparse (Abst nameA exprA)) ++ ")(" ++ (unparse exprB) ++ ")"
+unparse (Appl exprA (Var nameB)) = (unparse exprA) ++ " " ++ nameB
+unparse (Appl exprA exprB) = (unparse exprA) ++ "(" ++ (unparse exprB) ++ ")"
+
+indentPrettyExpr :: Expression -> String -> String
+indentPrettyExpr (Var name) indent = indent ++ "(Var " ++ (show name) ++ ")"
+indentPrettyExpr (Abst name expr) indent =
+  indent ++ "(Abst " ++ (show name) ++ "\n"
+  ++ (indentPrettyExpr expr (indent ++ " ")) ++ "\n" ++ indent ++ ")"
+indentPrettyExpr (Appl exprA exprB) indent =
+  indent ++ "(Appl \n" ++  (indentPrettyExpr exprA (indent ++ " ")) ++ "\n"
+  ++ (indentPrettyExpr exprB (indent ++ " ")) ++ "\n" ++ indent ++ ")"
+
+isChurchNumBody :: String -> String -> Int -> Expression -> Maybe Int
+isChurchNumBody f x i (Appl (Var a) expr) =
+  if f == a then isChurchNumBody f x (i + 1) expr else Nothing
+isChurchNumBody f x i (Var a) = if x == a then Just i else Nothing
+isChurchNumBody f x i expr = Nothing
+
+restoreNums :: Expression -> Expression
+restoreNums (Abst a (Var b)) = if a == b then (Var "1") else (Abst a (Var b))
+restoreNums (Abst a (Abst b body)) =
+  maybe (Abst a (restoreNums (Abst b body))) (\n -> Var (show n))
+      (isChurchNumBody a b 0 body)
+restoreNums (Var a) = Var a
+restoreNums (Abst a body) = Abst a (restoreNums body)
+restoreNums (Appl exprA exprB) = Appl (restoreNums exprA) (restoreNums exprB)
+
+prettyExpr :: Expression -> String
+prettyExpr expr = indentPrettyExpr expr ""
+
+-- Semantics ------------------------------------------------------------------
